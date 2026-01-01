ae7fd3c86c7b825d1ba2e99bd957df35b5853f26
Steve <steve.gitprofile@outlook.com>
2026-01-01 18:05:18 +0200

    chore: add shadcn and components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Noto_Sans } from 'next/font/google';
 
 import './globals.css';
+
+const notoSans = Noto_Sans({ variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className={notoSans.variable} lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
